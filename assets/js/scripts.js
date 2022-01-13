db70b3d6c4348d37e409f5e8a8afe802c03e52da
Bruno Staine <brunostaine@hotmail.com>
2022-01-13 16:59:17 -0300

    javascript da página

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+var currentNumberWrapper = document.getElementById("currentNumber")
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if (currentNumber > -1) {
+        document.getElementById('currentNumber').style.color = 'white'
+    }
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+    if (currentNumber < 0) {
+        document.getElementById('currentNumber').style.color = 'red'
+    } 
+}
